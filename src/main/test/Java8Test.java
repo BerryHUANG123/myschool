87a10a7302cf34605b2a35fc437be6c49a25aa5e
黄进阳 <huangjinyang@dianxiaomi.com>
2018-07-12 19:55:06 +0800

    [update]JAVA8TEST.

--- src/main/test/Java8Test.java
+++ src/main/test/Java8Test.java
@@ -1,15 +1,21 @@
+import com.alibaba.druid.support.json.JSONUtils;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import org.apache.commons.lang.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
+import us.codecraft.webmagic.selector.Json;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.toList;
 
 public class Java8Test extends BaseTest {
@@ -46,8 +52,8 @@ public class Java8Test extends BaseTest {
     public void testPredicate() {
         Apple apple = new Apple();
         apple.setWeight(500);
-        System.out.println(checkApple(apple,new AppleWeightPredicate(),250.0));
-        System.out.println(checkApple(apple,new AppleWeightPredicate(),5000.0));
+        System.out.println(checkApple(apple, new AppleWeightPredicate(), 250.0));
+        System.out.println(checkApple(apple, new AppleWeightPredicate(), 5000.0));
     }
 
     public static <T> boolean checkApple(Apple apple, ApplePredicate applePredicate, T t) {
@@ -55,25 +61,63 @@ public class Java8Test extends BaseTest {
     }
 
     @Test
-    public void testLambda(){
+    public void testLambda() {
         Apple realApple = new Apple();
         realApple.setWeight(500);
-        ApplePredicate<Double> applePredicate = (apple,standWeight)-> apple.getWeight()>standWeight;
-        System.out.println(applePredicate.test(realApple,300.0));
+        ApplePredicate<Double> applePredicate = (apple, standWeight) -> apple.getWeight() > standWeight;
+        System.out.println(applePredicate.test(realApple, 300.0));
     }
 
     @Test
-    public void test1(){
+    public void test1() {
         Predicate<String> p = StringUtils::isNotBlank;
     }
 
-    public Callable<String> fetch(){
+    public Callable<String> fetch() {
         return () -> "1111";
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         int[] intArr = new int[]{1, 2, 3, 4, 5};
-        System.out.println(Arrays.stream(intArr).map(operand -> operand*operand).boxed().collect(toList()));
+        System.out.println(Arrays.stream(intArr).map(operand -> operand * operand).boxed().collect(toList()));
     }
+
+    @Test
+    public void test3() {
+        int[] intArr = new int[]{1, 2, 3, 4, 5};
+        System.out.println(Arrays.stream(intArr).filter(value -> value == 6).findAny().orElse(0));
+    }
+
+    @Test
+    public void test4() {
+        int[] intArr = new int[]{};
+        System.out.println(Arrays.stream(intArr).min().orElse(-1));
+    }
+
+    @Test
+    public void test5() {
+        Integer[] intArr = new Integer[]{1, 2, null, 3, 5};
+        int max;
+        Arrays.stream(intArr).filter(Objects::nonNull).mapToInt(value -> value).max().orElseGet(() -> 0);
+    }
+
+    @Test
+    public void test6() {
+        Stream<int[]> pythagoreanTriples =
+                IntStream.rangeClosed(1, 100).boxed()
+                        .flatMap(a ->
+                                IntStream.rangeClosed(a, 100)
+                                        .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
+                                        .mapToObj(b ->
+                                                new int[]{a, b, (int) Math.sqrt(a * a + b * b)})
+                        );
+    }
+
+    @Test
+    public void test7() {
+        long a = Stream.iterate(2, n -> n * n).collect(counting());
+        System.out.println(a);
+    }
+
 }
---
+++ src/main/test/MyCollector.java
@@ -0,0 +1,46 @@
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+/**
+ * @author :sujinpeng
+ * @Date :2018/7/12
+ * @Time :19:36
+ * @Description :我的收集器
+ */
+public class MyCollector<T> implements Collector {
+
+    @Override
+    public Supplier<List<T>> supplier() {
+        return Lists::newArrayList;
+    }
+
+    @Override
+    public BiConsumer<List<T>, T> accumulator() {
+        return List::add;
+    }
+
+    @Override
+    public BinaryOperator<List<T>> combiner() {
+        return (list1, list2) -> {
+            list1.addAll(list2);
+            return list1;
+        };
+    }
+
+    @Override
+    public Function<List<T>, List<T>> finisher() {
+        return Function.identity();
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return null;
+    }
+}
